21315bc681b246cbae0e8fe08b6bd592584a1e52
Prashanth <iamprashanthchowdary@gmail.com>
2026-09-14 21:29:45 +0530

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.js
@@ -0,0 +1,65 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+   findMedianSortedArrays(nums1, nums2) {
+    // Always binary search on the smaller array
+    if (nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
+
+    const m = nums1.length;
+    const n = nums2.length;
+
+    let left = 0;
+    let right = m;
+
+    while (left <= right) {
+        // Partition nums1
+        const partition1 = Math.floor((left + right) / 2);
+
+        // Partition nums2 so left side has half of all elements
+        const partition2 = Math.floor((m + n + 1) / 2) - partition1;
+
+        const maxLeft1 = partition1 === 0
+            ? -Infinity
+            : nums1[partition1 - 1];
+
+        const minRight1 = partition1 === m
+            ? Infinity
+            : nums1[partition1];
+
+        const maxLeft2 = partition2 === 0
+            ? -Infinity
+            : nums2[partition2 - 1];
+
+        const minRight2 = partition2 === n
+            ? Infinity
+            : nums2[partition2];
+
+        // Correct partition
+        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            // Odd number of elements
+            if ((m + n) % 2 === 1) {
+                return Math.max(maxLeft1, maxLeft2);
+            }
+
+            // Even number of elements
+            return (
+                Math.max(maxLeft1, maxLeft2) +
+                Math.min(minRight1, minRight2)
+            ) / 2;
+        }
+
+        // nums1 partition is too far right
+        if (maxLeft1 > minRight2) {
+            right = partition1 - 1;
+        } else {
+            // nums1 partition is too far left
+            left = partition1 + 1;
+        }
+    }
+}
+}
